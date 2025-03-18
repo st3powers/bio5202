7ec3a948611c6b918bb2225fb0717b84e570fe83
Stephen M. Powers <st3powers@gmail.com>
2025-03-18 13:52:18 -0500

    Update dates.R

--- dates.R
+++ dates.R
@@ -2,5 +2,5 @@
 library(tidyverse)
 PATH<-"https://raw.githubusercontent.com/waterfolk/waterfolkdata/main/samples.csv"
 
-data <- read_csv(PATH<-"https://raw.githubusercontent.com/waterfolk/waterfolkdata/main/samples.csv")
+data <- read_csv(PATH)
 
