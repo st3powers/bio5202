b04ba669dbf6f6102a0695b6797c73fe921fb5f0
stephen powers <st3powers@gmail.com>
2025-03-18 13:13:44 -0500

    added dates/R

---
+++ dates.R
@@ -0,0 +1,6 @@
+
+library(tidyverse)
+PATH<-"https://raw.githubusercontent.com/waterfolk/waterfolkdata/main/samples.csv"
+
+data <- read_csv(PATH<-"https://raw.githubusercontent.com/waterfolk/waterfolkdata/main/samples.csv")
+
